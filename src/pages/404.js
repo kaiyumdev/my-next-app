a9d0fe43d1524380fddeea2c09c81a87f6ae8f6d
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-10-29 12:12:01 +0600

    navigate with link component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,12 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 const ErrorPage = () => {
+  const router = useRouter();
+
+  setTimeout(() => {
+    router.push("/");
+  }, 5000);
   return (
     <div>
       <img
--- src/pages/news/index.js
+++ src/pages/news/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Button } from "antd";
+import Link from "next/link";
 
 const NewsPage = () => {
   return (
     <div>
       <h1>Welcome to news page</h1>
-      <Button type="primary">Primary Button</Button>
+      <Button type="primary">
+        <Link href="/">Home</Link>
+      </Button>
     </div>
   );
 };
