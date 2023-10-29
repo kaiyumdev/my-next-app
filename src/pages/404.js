691096dfded4239969ea57d86f597013841e963f
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-10-29 11:04:00 +0600

    implements catchallroutes, 404 error page and ant design successfully

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <img
+        width="100%"
+        src="https://colibriwp.com/blog/wp-content/uploads/2019/07/2488756.jpg"
+      ></img>
+    </div>
+  );
+};
+
+export default ErrorPage;
---
+++ src/pages/news/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "antd";
+
+const NewsPage = () => {
+  return (
+    <div>
+      <h1>Welcome to news page</h1>
+      <Button type="primary">Primary Button</Button>
+    </div>
+  );
+};
+
+export default NewsPage;
